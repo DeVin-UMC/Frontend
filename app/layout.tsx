df34a68a962139b056d43cb537b83d4b9c722371
wooho <woohobi@gmail.com>
2022-10-30 23:13:24 +0900

    🎉 initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-import './globals.css'
+import './globals.css';
+import React from 'react';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -14,5 +11,5 @@ export default function RootLayout({
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
