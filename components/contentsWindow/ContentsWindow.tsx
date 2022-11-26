d2ca98e7799bfa2ce3bf303644d76fd09e774195
정연한 <yt4307@gmail.com>
2022-11-26 18:39:30 +0900

    [Fixes] ContentsWindow 수정 중

--- components/contentsWindow/ContentsWindow.tsx
+++ components/contentsWindow/ContentsWindow.tsx
@@ -13,7 +13,18 @@ const cx = classNames.bind(styles);
 const ContentsWindow = ({ title, children }: Props) => {
   return (
     <div className={cx("container")}>
-      <div className={cx("window")} />
+      <div className={cx("window")}>
+        <div className={cx("main-contents")}>
+          {children}
+          <div className={cx("test-contents")}>test</div>
+          <div className={cx("test-contents")}>test</div>
+          <div className={cx("test-contents")}>test</div>
+          <div className={cx("test-contents")}>test</div>
+          <div className={cx("test-contents")}>test</div>
+          <div className={cx("test-contents")}>test</div>
+          <div className={cx("test-contents")}>test</div>
+        </div>
+      </div>
       <div className={cx("grid-line", "top")}>
         <Image className={cx("title")} src={title} alt="title" layout="fill" />
         <div className={cx("new-post")}>
@@ -28,12 +39,20 @@ const ContentsWindow = ({ title, children }: Props) => {
       <div className={cx("grid-line", "second")} />
       <div className={cx("grid-line", "third")} />
       <div className={cx("grid-line", "fourth")}>
+        <div className={cx("filter-container")}>
+          <div className={cx("filter-contents")}>분류</div>
+          <div className={cx("filter-contents")}>지역</div>
+          <div className={cx("filter-contents")}>기간</div>
+          <div className={cx("filter-contents")}>단계</div>
+          <div className={cx("filter-contents")}>사용 언어</div>
+        </div>
         <div className={cx("tag-container")}>
           <Tag>태그 테스트</Tag>
           <Tag>여러개 테스트</Tag>
           <Tag>여러개 테스트2</Tag>
           <Tag>여러개 테스트3</Tag>
         </div>
+        <div className={cx("search-bar")}>검색창</div>
       </div>
     </div>
   );
