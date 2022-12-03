f2711ce5206e584b71a2948c8b767e45359bd6b7
WooWan <47740690+WooWan@users.noreply.github.com>
2022-12-03 15:43:29 +0900

    Chore import queryclient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { QueryClient } from "@tanstack/react-query";
 import { AppProps } from "next/app";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 
